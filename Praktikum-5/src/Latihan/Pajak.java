47fb499c1052aca5d799492a60087deb451b1982
ZoeMohamed <zoemohamed31@gmail.com>
2025-03-23 21:16:36 +0700

    Menyelesaikan Modul Praktikum 5

---
+++ Praktikum-5/src/Latihan/Pajak.java
@@ -0,0 +1,11 @@
+/* Nama File : Pajak.java
+ * Deskripsi : Interface yang mendefinisikan kemampuan untuk menghitung pajak
+ * Pembuat   : Zoe Mohamed // 24060123140182
+ * Tanggal   : 23/03/2025
+ */
+package Latihan;
+
+public interface Pajak {
+    // Method untuk menghitung pajak yang harus dibayarkan
+    double hitungPajak();
+}
